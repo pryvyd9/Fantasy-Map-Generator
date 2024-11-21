1bd72edb6ee1a5da06557297baec6d154d63f88d
Prizrak9 <pavelgolotuk@gmail.com>
2024-11-21 22:52:51 +0200

    d

--- modules/io/export.js
+++ modules/io/export.js
@@ -925,8 +925,8 @@ function wrapInSvg(element, id, filename, {includeDefs, includeDefsRelief} = {})
     var d1 = document.getElementById("map").getElementsByTagName("defs")[0].cloneNode(true);
     var d2 = document.getElementById("defElements").getElementsByTagName("defs")[0].cloneNode(true);
     svg.appendChild(d1);
-    for(var d in d2.childNodes){
-      d1.appendChild(d);
+    for (let index = 0; index < d2.children.length; index++) {
+      d1.appendChild(d2.children[index]);
     }
   } else if (includeDefs){
     var defs = document.getElementById("map").getElementsByTagName("defs")[0].cloneNode(true);
